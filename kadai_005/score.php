281381f110de173d7120f5ce6f224698e0457998
kinako-motomura <158249308+kinako-motomura@users.noreply.github.com>
2024-02-29 16:57:06 +0900

    再提出の修正

--- kadai_005/score.php
+++ kadai_005/score.php
@@ -10,35 +10,25 @@
     <p>
         <?php
         // 変数に値を代入する
-       echo $score1 = 80;
-       echo '<br>';
-       echo $score2 = 60;
-       echo '<br>';
-       echo $score3 = 55;
-       echo '<br>';
-       echo $score4 = 40;
-       echo '<br>';
-       echo $score5 = 100;
-       echo '<br>';
-       echo $score6 = 25;
-       echo '<br>';
-       echo $score7 = 80;
-       echo '<br>';
-       echo $score8 = 95;
-       echo '<br>';
-       echo $score9 = 30;
-       echo '<br>';
-       echo $score10 = 60;
-       echo '<br>';
+        $score1 = 80;
+        $score2 = 60;
+        $score3 = 55;
+        $score4 = 40;
+        $score5 = 100;
+        $score6 = 25;
+        $score7 = 80;
+        $score8 = 95;
+        $score9 = 30;
+        $score10 = 60;
 
         //変数と数値を計算し、その結果を出力する(合計点)
-        //echo $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+       echo $total = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
 
          //改行する
          echo '<br>';
 
          //変数と数値を計算し、その結果を出力する(平均点)
-         echo ($score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10) / 10;
+        echo $average = ($score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10) / 10;
         ?>
     </p>
 </body>
